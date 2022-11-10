f445d8307f7bd9c34ca31d86f2e845c4d5fc4e2c
kklee0930 <kklee0930@gmail.com>
2022-11-10 22:58:43 +0900

    articles/index.html 카드 및 푸터 작업

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,8 @@ app_name = "articles"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("detail/", views.detail, name="detail"),
+    path("shop/", views.shop, name="shop"),
+    path("cost/", views.cost, name="cost"),
     # path('', views.AllListView.as_view(), name='all_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-
 def index(request):
     return render(request, "index.html")
+
+def detail(request):
+    return render(request, "detail.html")
+
+def shop(request):
+    return render(request, "shop.html")
+
+def cost(request):
+    return render(request, "cost.html")
